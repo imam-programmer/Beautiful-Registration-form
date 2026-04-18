ed59fb328fcc81a42d6d7d8ae108dd8677f58c93
imam-programmer <imamprogrammer09@gmail.com>
2026-04-18 20:14:27 +0600

    js setup done

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+let firstname=document.querySelector('#firstname');
+let lastname=document.querySelector('#lastname');
+let email=document.querySelector('#email');
+let password=document.querySelector('#password');
+let submit=document.querySelector('#submit')
 
+submit.addEventListener('click',(event)=>{
+    event.preventDefault()
+    if(!firstname.value){
+        firstname.style.border='2px solid red'
+    }else{
+        firstname.style.border='none'
+    }
+    if(!lastname.value){
+        lastname.style.border='2px solid red'
+    }else{
+        lastname.style.border='none'
+    }
+    if(!email.value){
+        email.style.border='2px solid red'
+    }else{
+        email.style.border='none'
+    }
+    if(!password.value){
+        password.style.border='2px solid red'
+    }else{
+        password.style.border='none'
+    }
+  
+   
+})
